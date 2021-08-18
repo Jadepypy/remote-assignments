4583e0072e5f28483ae85fc6f21a2f490c26d6a8
Jadepypy <questiongirl829@gmail.com>
2021-08-18 21:15:08 +0800

    Complete ajax request to proxy server

--- Week-4/Assignment-2/proxyserver/app.js
+++ Week-4/Assignment-2/proxyserver/app.js
@@ -2,30 +2,24 @@ const http = require("http");
 const express = require('express');
 const app = express();
 
-http
+app.get('/', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  http
   .get("http://13.230.176.178:4000/api/1.0/remote-w4-data", resp => {
     let data = "";
 
-    // A chunk of data has been recieved.
     resp.on("data", chunk => {
       data += chunk;
     });
 
-    // The whole response has been received. Print out the result.
     resp.on("end", () => {
-      data = JSON.parse(data)
-      console.log(data);
+      data = JSON.parse(data);
+      res.send(data);
     });
   })
   .on("error", err => {
     console.log("Error: " + err.message);
   });
+})
 
-// app.get('/', (req, res) => {
-//   let data = '';
-
-
-//   res.send('?');
-// })
-
-// app.listen(3000);
+app.listen(3000);
