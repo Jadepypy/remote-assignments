c7c83de8f62884a6f064d9c5a4d4c4eae7a31bd9
Jadepypy <questiongirl829@gmail.com>
2021-08-20 09:11:55 +0800

    Complete home page layout

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,3 +1,27 @@
 const express = require('express');
-const app = express();
+const mysqlx = require('@mysql/xdevapi');
+const bodyParser = require('body-parser');
 
+const config = {
+    password: 'mysql123',
+    user: 'root',
+    host: 'localhost',
+    port: 33060,
+    schema: 'assignment'
+};
+
+mysqlx.getSession(config)
+    .then(session => {
+        console.log(session.inspect()); 
+    });
+
+const app = express();
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.listen(3000);
