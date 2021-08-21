3bc5f6a8205857eb2b67b11ce60607bb3c873042
Jadepypy <questiongirl829@gmail.com>
2021-08-22 00:57:38 +0800

    Complete Assignment-4

---
+++ Week-4/Assignment-4/practice.js
@@ -0,0 +1,12 @@
+function delayedResultPromise(n1, n2, delayTime) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(n1 + n2), delayTime);
+  })
+}
+delayedResultPromise(4, 5 , 3000).then(console.log) // 9 (4+5) will be shown in the console after 3 seconds
+
+async function main() {
+  let result = await delayedResultPromise(4, 5 ,3000);
+  console.log(result);
+}
+main() // result will be shown in the console after <delayTime> seconds
